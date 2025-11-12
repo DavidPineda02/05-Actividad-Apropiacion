702e8931d9cec9e441dd654a092113fc2603b1ea
DavidPineda02 <davxpa.02@gmail.com>
2025-11-12 15:37:21 -0500

    Agregando Modularizacion a los ejercicios

---
+++ app.js
@@ -0,0 +1,32 @@
+//Importaciones de los modulos 
+
+let opcion = "";
+
+while (opcion !== "0"){
+    opcion = prompt();
+
+    switch (opcion) {
+        case "1":
+            
+            break;
+
+        case "2":
+            
+            break;
+
+        case "3":
+            
+            break;
+
+        case "4":
+            
+            break;
+        
+        case "0":
+            alert("Gracias...");
+            break;
+
+        default:
+            alert("Opcion no valida. ingresa un numero entre 1 o 0");
+    }
+}
